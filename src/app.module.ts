c8374b24e12b9154c304fa2d0b91dbaa14006272
Matthias Klebe <matthias.klebe@1000shapes.com>
2023-06-25 22:23:40 +0200

    sets up database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,61 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+import { DataSource } from 'typeorm';
 
+const postgresConnectionOptions: PostgresConnectionOptions = {
+  type: 'postgres',
+  logging: true,
+  entities: [],
+  migrations: [],
+  migrationsRun: true,
+};
+
+function getPostgresDataSource(config) {
+  return new DataSource({ ...postgresConnectionOptions, ...config });
+}
+
+function createTypeOrmConfig(
+  config: ConfigService,
+  databaseConnectionString: string,
+): Partial<TypeOrmModuleOptions> {
+  const defaultDatabaseUrl = config.getOrThrow(databaseConnectionString);
+  const useSSL = false;
+  const typeOrmConfig: Partial<TypeOrmModuleOptions> = {
+    url: defaultDatabaseUrl,
+    logging: 'all',
+    retryDelay: 10_000,
+    synchronize: false,
+  };
+  if (useSSL) {
+    Object.assign(typeOrmConfig, { ssl: { rejectUnauthorized: false } });
+  }
+
+  return typeOrmConfig;
+}
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      envFilePath: ['.env'],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) =>
+        createTypeOrmConfig(config, 'DB_CONNECTION_STRING'),
+      dataSourceFactory: (config) => {
+        return getPostgresDataSource(config).initialize();
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
