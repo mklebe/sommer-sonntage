0c9e1c16506839631cfbe783c9664445f87daaaf
Matthias Klebe <matthias.klebe@1000shapes.com>
2023-07-16 15:38:44 +0200

    now cors?

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  console.log('Start application');
+  const app = await NestFactory.create(AppModule, { cors: true });
+
   await app.listen(3000);
 }
 bootstrap();
