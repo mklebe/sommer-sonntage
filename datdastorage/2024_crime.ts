d6e44b38534f0ca4cc4d7bd5b7bdf94ec462b3af
Matthias Klebe <matthias.klebe@gmail.com>
2024-07-28 20:08:37 +0200

    Update 2024_crime.ts

--- datdastorage/2024_crime.ts
+++ datdastorage/2024_crime.ts
@@ -1,182 +1,182 @@
 export const crime_songs = [
-    {"placement": 1, "artist": "Johnny Cash", "song": "Folsom Prison Blues"},
-    {"placement": 2, "artist": "Nick Cave And The Bad Seeds ft. Kylie Minogue", "song": "Where The Wild Roses Grow"},
-    {"placement": 3, "artist": "Bob Marley And The Wailers", "song": "I Shot The Sheriff"},
-    {"placement": 4, "artist": "Talking Heads", "song": "Psycho Killer"},
-    {"placement": 5, "artist": "Falco", "song": "Jeanny (Part 1)"},
-    {"placement": 6, "artist": "Brigitte Bardot & Serge Gainsbourg", "song": "Bonnie And Clyde"},
-    {"placement": 7, "artist": "Queen", "song": "Bohemian Rhapsody"},
-    {"placement": 8, "artist": "The Boomtown Rats", "song": "I Don't Like Mondays"},
-    {"placement": 9, "artist": "PJ Harvey", "song": "Down By The Water"},
-    {"placement": 10, "artist": "The Clash", "song": "I Fought The Law"},
-    {"placement": 11, "artist": "The Jimi Hendrix Experience", "song": "Hey Joe"},
-    {"placement": 12, "artist": "Judas Priest", "song": "Breaking The Law"},
-    {"placement": 13, "artist": "The Clash", "song": "Bankrobber"},
-    {"placement": 14, "artist": "Bob Dylan", "song": "Hurricane"},
-    {"placement": 15, "artist": "The Killers", "song": "Jenny Was A Friend Of Mine"},
-    {"placement": 16, "artist": "Sophie Ellis-Bextor", "song": "Murder On The Dancefloor"},
-    {"placement": 17, "artist": "Foster The People", "song": "Pumped Up Kicks"},
-    {"placement": 18, "artist": "Body Count", "song": "Cop Killer"},
-    {"placement": 19, "artist": "Eminem ft. Dido", "song": "Stan"},
-    {"placement": 20, "artist": "Nancy Sinatra", "song": "Bang Bang (My Baby Shot Me Down)"},
-    {"placement": 21, "artist": "Ween", "song": "Buenas Tardes Amigo"},
-    {"placement": 22, "artist": "Bruce Springsteen", "song": "Nebraska"},
-    {"placement": 23, "artist": "Jane's Addiction", "song": "Been Caught Stealing"},
-    {"placement": 24, "artist": "The Smiths", "song": "Shoplifters Of The World Unite"},
-    {"placement": 25, "artist": "Billie Holiday", "song": "Strange Fruit"},
-    {"placement": 26, "artist": "Michael Jackson", "song": "Smooth Criminal"},
-    {"placement": 27, "artist": "Nick Cave And The Bad Seeds ft. PJ Harvey", "song": "Henry Lee"},
-    {"placement": 28, "artist": "Lotte Lenya (Brecht/Weill)", "song": "Die Moritat Von Mackie Messer"},
-    {"placement": 29, "artist": "Rage Against The Machine", "song": "Killing In The Name"},
-    {"placement": 30, "artist": "Superpunk", "song": "Neue Zähne Für Meinen Bruder Und Mich"},
-    {"placement": 31, "artist": "Nick Cave And The Bad Seeds", "song": "Stagger Lee"},
-    {"placement": 32, "artist": "Sufjan Stevens", "song": "John Wayne Gacy, Jr."},
-    {"placement": 33, "artist": "Nirvana", "song": "Polly"},
-    {"placement": 34, "artist": "Pearl Jam", "song": "Jeremy"},
-    {"placement": 35, "artist": "The Adverts", "song": "Gary Gilmore's Eyes"},
-    {"placement": 36, "artist": "Interpol", "song": "Evil"},
-    {"placement": 37, "artist": "The Slits", "song": "Shoplifting"},
-    {"placement": 38, "artist": "The Cranberries", "song": "Zombie"},
-    {"placement": 39, "artist": "Aerosmith", "song": "Janie's Got A Gun"},
-    {"placement": 40, "artist": "Hazy Osterwald Sextett", "song": "Kriminaltango"},
-    {"placement": 41, "artist": "Siouxsie And The Banshees", "song": "Night Shift"},
-    {"placement": 42, "artist": "Die Ärzte", "song": "El Cattivo"},
-    {"placement": 43, "artist": "Thin Lizzy", "song": "Jailbreak"},
-    {"placement": 44, "artist": "The Smiths", "song": "Suffer Little Children"},
-    {"placement": 45, "artist": "Fun Lovin' Criminals", "song": "Scooby Snacks"},
-    {"placement": 46, "artist": "Bob Dylan", "song": "Murder Most Foul"},
-    {"placement": 47, "artist": "Coolio ft. L.V.", "song": "Gangsta's Paradise"},
-    {"placement": 48, "artist": "Curtis Mayfield", "song": "Pusherman"},
-    {"placement": 49, "artist": "Nick Cave And The Bad Seeds", "song": "The Mercy Seat"},
-    {"placement": 50, "artist": "M.I.A.", "song": "Paper Planes"},
-    {"placement": 51, "artist": "Neil Young", "song": "Crime In The City"},
-    {"placement": 52, "artist": "Screaming Lord Sutch", "song": "Jack The Ripper"},
-    {"placement": 53, "artist": "U2", "song": "Sunday Bloody Sunday"},
-    {"placement": 54, "artist": "Die Toten Hosen", "song": "Bonnie & Clyde"},
-    {"placement": 55, "artist": "The Clash", "song": "The Guns Of Brixton"},
-    {"placement": 56, "artist": "SZA", "song": "Kill Bill"},
-    {"placement": 57, "artist": "Slayer", "song": "Dead Skin Mask"},
-    {"placement": 58, "artist": "Erste Allgemeine Verunsicherung", "song": "Ba-Ba Banküberfall"},
-    {"placement": 59, "artist": "The Rolling Stones", "song": "Midnight Rambler"},
-    {"placement": 60, "artist": "Hildegard Knef", "song": "Mackie Messer (Mack The Knife)"},
-    {"placement": 61, "artist": "The Clash", "song": "Police & Thieves"},
-    {"placement": 62, "artist": "Nirvana", "song": "Where Did You Sleep Last Night"},
-    {"placement": 63, "artist": "Taylor Swift ft. Haim", "song": "No Body, No Crime"},
-    {"placement": 64, "artist": "The Jam", "song": "Down In The Tube Station At Midnight"},
-    {"placement": 65, "artist": "Fuffifufzich", "song": "Heartbreakerei"},
-    {"placement": 66, "artist": "The Bobby Fuller Four", "song": "I Fought The Law"},
-    {"placement": 67, "artist": "Tristan Brusch", "song": "Am Herz Vorbei"},
-    {"placement": 68, "artist": "Tom Waits", "song": "Georgia Lee"},
-    {"placement": 69, "artist": "Nina Simone", "song": "Mississippi Goddam"},
-    {"placement": 70, "artist": "The Saints", "song": "Swing For The Crime"},
-    {"placement": 71, "artist": "Slayer", "song": "Angel Of Death"},
-    {"placement": 72, "artist": "Grim104", "song": "Ich Töte Anders Breivik"},
-    {"placement": 73, "artist": "Prefab Sprout", "song": "The Best Jewel Thief In The World"},
-    {"placement": 74, "artist": "Kendrick Lamar", "song": "The Art Of Peer Pressure"},
-    {"placement": 75, "artist": "Antilopen Gang", "song": "Enkeltrick"},
-    {"placement": 76, "artist": "Nick Cave And The Bad Seeds", "song": "Red Right Hand"},
-    {"placement": 77, "artist": "Extrabreit", "song": "Kleptomanie"},
-    {"placement": 78, "artist": "Suzanne Vega", "song": "Luka"},
-    {"placement": 79, "artist": "Supergrass", "song": "Caught By The Fuzz"},
-    {"placement": 80, "artist": "Prince Buster", "song": "Judge Dread"},
+    {"placement": 1, "artist": "Johnny Cash", "title": "Folsom Prison Blues"},
+    {"placement": 2, "artist": "Nick Cave And The Bad Seeds ft. Kylie Minogue", "title": "Where The Wild Roses Grow"},
+    {"placement": 3, "artist": "Bob Marley And The Wailers", "title": "I Shot The Sheriff"},
+    {"placement": 4, "artist": "Talking Heads", "title": "Psycho Killer"},
+    {"placement": 5, "artist": "Falco", "title": "Jeanny (Part 1)"},
+    {"placement": 6, "artist": "Brigitte Bardot & Serge Gainsbourg", "title": "Bonnie And Clyde"},
+    {"placement": 7, "artist": "Queen", "title": "Bohemian Rhapsody"},
+    {"placement": 8, "artist": "The Boomtown Rats", "title": "I Don't Like Mondays"},
+    {"placement": 9, "artist": "PJ Harvey", "title": "Down By The Water"},
+    {"placement": 10, "artist": "The Clash", "title": "I Fought The Law"},
+    {"placement": 11, "artist": "The Jimi Hendrix Experience", "title": "Hey Joe"},
+    {"placement": 12, "artist": "Judas Priest", "title": "Breaking The Law"},
+    {"placement": 13, "artist": "The Clash", "title": "Bankrobber"},
+    {"placement": 14, "artist": "Bob Dylan", "title": "Hurricane"},
+    {"placement": 15, "artist": "The Killers", "title": "Jenny Was A Friend Of Mine"},
+    {"placement": 16, "artist": "Sophie Ellis-Bextor", "title": "Murder On The Dancefloor"},
+    {"placement": 17, "artist": "Foster The People", "title": "Pumped Up Kicks"},
+    {"placement": 18, "artist": "Body Count", "title": "Cop Killer"},
+    {"placement": 19, "artist": "Eminem ft. Dido", "title": "Stan"},
+    {"placement": 20, "artist": "Nancy Sinatra", "title": "Bang Bang (My Baby Shot Me Down)"},
+    {"placement": 21, "artist": "Ween", "title": "Buenas Tardes Amigo"},
+    {"placement": 22, "artist": "Bruce Springsteen", "title": "Nebraska"},
+    {"placement": 23, "artist": "Jane's Addiction", "title": "Been Caught Stealing"},
+    {"placement": 24, "artist": "The Smiths", "title": "Shoplifters Of The World Unite"},
+    {"placement": 25, "artist": "Billie Holiday", "title": "Strange Fruit"},
+    {"placement": 26, "artist": "Michael Jackson", "title": "Smooth Criminal"},
+    {"placement": 27, "artist": "Nick Cave And The Bad Seeds ft. PJ Harvey", "title": "Henry Lee"},
+    {"placement": 28, "artist": "Lotte Lenya (Brecht/Weill)", "title": "Die Moritat Von Mackie Messer"},
+    {"placement": 29, "artist": "Rage Against The Machine", "title": "Killing In The Name"},
+    {"placement": 30, "artist": "Superpunk", "title": "Neue Zähne Für Meinen Bruder Und Mich"},
+    {"placement": 31, "artist": "Nick Cave And The Bad Seeds", "title": "Stagger Lee"},
+    {"placement": 32, "artist": "Sufjan Stevens", "title": "John Wayne Gacy, Jr."},
+    {"placement": 33, "artist": "Nirvana", "title": "Polly"},
+    {"placement": 34, "artist": "Pearl Jam", "title": "Jeremy"},
+    {"placement": 35, "artist": "The Adverts", "title": "Gary Gilmore's Eyes"},
+    {"placement": 36, "artist": "Interpol", "title": "Evil"},
+    {"placement": 37, "artist": "The Slits", "title": "Shoplifting"},
+    {"placement": 38, "artist": "The Cranberries", "title": "Zombie"},
+    {"placement": 39, "artist": "Aerosmith", "title": "Janie's Got A Gun"},
+    {"placement": 40, "artist": "Hazy Osterwald Sextett", "title": "Kriminaltango"},
+    {"placement": 41, "artist": "Siouxsie And The Banshees", "title": "Night Shift"},
+    {"placement": 42, "artist": "Die Ärzte", "title": "El Cattivo"},
+    {"placement": 43, "artist": "Thin Lizzy", "title": "Jailbreak"},
+    {"placement": 44, "artist": "The Smiths", "title": "Suffer Little Children"},
+    {"placement": 45, "artist": "Fun Lovin' Criminals", "title": "Scooby Snacks"},
+    {"placement": 46, "artist": "Bob Dylan", "title": "Murder Most Foul"},
+    {"placement": 47, "artist": "Coolio ft. L.V.", "title": "Gangsta's Paradise"},
+    {"placement": 48, "artist": "Curtis Mayfield", "title": "Pusherman"},
+    {"placement": 49, "artist": "Nick Cave And The Bad Seeds", "title": "The Mercy Seat"},
+    {"placement": 50, "artist": "M.I.A.", "title": "Paper Planes"},
+    {"placement": 51, "artist": "Neil Young", "title": "Crime In The City"},
+    {"placement": 52, "artist": "Screaming Lord Sutch", "title": "Jack The Ripper"},
+    {"placement": 53, "artist": "U2", "title": "Sunday Bloody Sunday"},
+    {"placement": 54, "artist": "Die Toten Hosen", "title": "Bonnie & Clyde"},
+    {"placement": 55, "artist": "The Clash", "title": "The Guns Of Brixton"},
+    {"placement": 56, "artist": "SZA", "title": "Kill Bill"},
+    {"placement": 57, "artist": "Slayer", "title": "Dead Skin Mask"},
+    {"placement": 58, "artist": "Erste Allgemeine Verunsicherung", "title": "Ba-Ba Banküberfall"},
+    {"placement": 59, "artist": "The Rolling Stones", "title": "Midnight Rambler"},
+    {"placement": 60, "artist": "Hildegard Knef", "title": "Mackie Messer (Mack The Knife)"},
+    {"placement": 61, "artist": "The Clash", "title": "Police & Thieves"},
+    {"placement": 62, "artist": "Nirvana", "title": "Where Did You Sleep Last Night"},
+    {"placement": 63, "artist": "Taylor Swift ft. Haim", "title": "No Body, No Crime"},
+    {"placement": 64, "artist": "The Jam", "title": "Down In The Tube Station At Midnight"},
+    {"placement": 65, "artist": "Fuffifufzich", "title": "Heartbreakerei"},
+    {"placement": 66, "artist": "The Bobby Fuller Four", "title": "I Fought The Law"},
+    {"placement": 67, "artist": "Tristan Brusch", "title": "Am Herz Vorbei"},
+    {"placement": 68, "artist": "Tom Waits", "title": "Georgia Lee"},
+    {"placement": 69, "artist": "Nina Simone", "title": "Mississippi Goddam"},
+    {"placement": 70, "artist": "The Saints", "title": "Swing For The Crime"},
+    {"placement": 71, "artist": "Slayer", "title": "Angel Of Death"},
+    {"placement": 72, "artist": "Grim104", "title": "Ich Töte Anders Breivik"},
+    {"placement": 73, "artist": "Prefab Sprout", "title": "The Best Jewel Thief In The World"},
+    {"placement": 74, "artist": "Kendrick Lamar", "title": "The Art Of Peer Pressure"},
+    {"placement": 75, "artist": "Antilopen Gang", "title": "Enkeltrick"},
+    {"placement": 76, "artist": "Nick Cave And The Bad Seeds", "title": "Red Right Hand"},
+    {"placement": 77, "artist": "Extrabreit", "title": "Kleptomanie"},
+    {"placement": 78, "artist": "Suzanne Vega", "title": "Luka"},
+    {"placement": 79, "artist": "Supergrass", "title": "Caught By The Fuzz"},
+    {"placement": 80, "artist": "Prince Buster", "title": "Judge Dread"},
     {
         "placement": 81,
         "artist": "Rod Stewart",
-        "song": "The Killing Of Georgie (Part I And II)"
+        "title": "The Killing Of Georgie (Part I And II)"
     },
     {
         "placement": 82,
         "artist": "Neil Young & Crazy Horse",
-        "song": "Down By The River"
+        "title": "Down By The River"
     },
     {
         "placement": 83,
         "artist": "Bob Dylan",
-        "song": "The Lonesome Death Of Hattie Carroll"
+        "title": "The Lonesome Death Of Hattie Carroll"
     },
     {
         "placement": 84,
         "artist": "Viagra Boys",
-        "song": "Baby Criminal"
+        "title": "Baby Criminal"
     },
     {
         "placement": 85,
         "artist": "Judas Priest",
-        "song": "The Ripper"
+        "title": "The Ripper"
     },
     {
         "placement": 86,
         "artist": "The Mighty Mighty Bosstones",
-        "song": "Mr. Moran"
+        "title": "Mr. Moran"
     },
     {
         "placement": 87,
         "artist": "Bob Dylan",
-        "song": "The Death Of Emmett Till"
+        "title": "The Death Of Emmett Till"
     },
     {
         "placement": 88,
         "artist": "The Cure",
-        "song": "Killing An Arab"
+        "title": "Killing An Arab"
     },
     {
         "placement": 89,
         "artist": "The Beatles",
-        "song": "Maxwell's Silver Hammer"
+        "title": "Maxwell's Silver Hammer"
     },
     {
         "placement": 90,
         "artist": "Cypress Hill",
-        "song": "Insane In The Brain"
+        "title": "Insane In The Brain"
     },
     {
         "placement": 91,
         "artist": "Sonic Youth",
-        "song": "Shoot"
+        "title": "Shoot"
     },
     {
         "placement": 92,
         "artist": "Tom Jones",
-        "song": "Delilah"
+        "title": "Delilah"
     },
     {
         "placement": 93,
         "artist": "Soko",
-        "song": "I’ll Kill Her"
+        "title": "I’ll Kill Her"
     },
     {
         "placement": 94,
         "artist": "Body Count",
-        "song": "Body Count"
+        "title": "Body Count"
     },
     {
         "placement": 95,
         "artist": "Dead Kennedys",
-        "song": "Police Truck"
+        "title": "Police Truck"
     },
     {
         "placement": 96,
         "artist": "Bob Dylan",
-        "song": "Ballad Of Hollis Brown"
+        "title": "Ballad Of Hollis Brown"
     },
     {
         "placement": 97,
         "artist": "Johnny Cash",
-        "song": "San Quentin"
+        "title": "San Quentin"
     },
     {
         "placement": 98,
         "artist": "Merle Haggard",
-        "song": "Mama Cried"
+        "title": "Mama Cried"
     },
     {
         "placement": 99,
         "artist": "Nina Simone",
-        "song": "Strange Fruit"
+        "title": "Strange Fruit"
     },
     {
         "placement": 100,
         "artist": "Blondie",
-        "song": "One Way Or Another"
+        "title": "One Way Or Another"
     }
 ];
